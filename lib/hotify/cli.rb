31aed5468fd791201d9dcb39cd5b48b062fbe85f
nishihata <nishihata@pepabo.com>
2020-05-21 18:32:01 +0900

    refactoring add remove method

--- lib/hotify/cli.rb
+++ lib/hotify/cli.rb
@@ -30,18 +30,12 @@ module Hotify
     def add_role_by_yaml(file_path)
       role_file = YAML.load_file(file_path)
       hotify_role = Hotify::Role.new
-
       role_file.each do | role_name, user_emails |
         user_emails.each do | user_email |
           user = Hotify::Users.find_by(email: user_email)
           user_role_names = hotify_role.roles_from(user: user).map{|user_role| user_role.name}
-
-          if user_role_names.include?(role_name)
-            puts "#{user.email} is already included in #{role_name}"
-            next
-          else
-            hotify_role.add_role(user, hotify_role.find_by_name(role_name))
-            puts "#{user.email} added to #{role_name}"
+          unless user_role_names.include?(role_name)
+            add!(user, role_name)
           end
         end
       end
@@ -53,17 +47,23 @@ module Hotify
       onelogin_roles = hotify_role.role_in_user
       onelogin_roles.each do | role_name, users |
         users.each do | user |
-          if role_file[role_name].include?(user.email)
-            puts "#{user.email} is included in #{role_name}"
-            next
-          else
-            puts "#{user.email} removed by #{role_name}"
-            user = Hotify::Users.find_by(email: user.email)
-            role = hotify_role.find_by_name(role_name)
-            hotify_role.leave_role(user, role)
+          unless role_file[role_name].include?(user.email)
+            remove!(user, role_name)
           end
         end
       end
+    end
+
+    def add!(user, role_name)
+      hotify_role = Hotify::Role.new
+      hotify_role.add_role(user, hotify_role.find_by_name(role_name))
+      puts "#{user.email} added to #{role_name}"
+    end
+
+    def remove!(user, role_name)
+      hotify_role = Hotify::Role.new
+      hotify_role.leave_role(user, hotify_role.find_by_name(role_name))
+      puts "#{user.email} removed by #{role_name}"
     end
 
     def role_in_user_dump(role_in_user)
